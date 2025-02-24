ac4c9cc402a938efedee0d2a35d4ba04be31d456
devrahulbanjara <rdbanjara07@gmail.com>
2025-02-24 12:44:17 +0545

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version = '0.0.0',
+    author = "Rahul Dev Banjara",
+    author_email = "rdbanjara07@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
